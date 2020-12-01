96c998a723ff15c309f918b7295b237e9881bb66
Neggie <aarondk1@hotmail.com>
2020-12-01 19:02:22 +0100

    Day1 - Added Part2 with hashset alternative

--- src/main/kotlin/challenges/day1/day1.kt
+++ src/main/kotlin/challenges/day1/day1.kt
@@ -3,20 +3,24 @@ package challenges.day1
 import java.io.File
 import kotlin.system.measureTimeMillis
 
-fun execute(){
-    val t = measureTimeMillis {
-        val part1Lines = getLines(getResourcePath("day1.txt"))
-        val t1 = measureTimeMillis {
-            println("Part 1: ${part1(part1Lines)}")
-        }
-        println(t1)
-        val t2 = measureTimeMillis {
-            println("Part 1 with hashset: ${part1HashSet(part1Lines)}")
-        }
-        println(t2)
+fun execute() {
+    val part1Lines = getLines(getResourcePath("day1.txt"))
+    val t1 = measureTimeMillis {
+        println("Part 1: ${part1(part1Lines)}")
+    }
+    println("Part 1 quadratic took: ${t1}")
+    val t2 = measureTimeMillis {
+        println("Part 1: ${part1HashSet(part1Lines)}")
+    }
+    println("Part 1 w/ hashset took: ${t2}")
+    val t3 = measureTimeMillis {
         println("Part 2: ${part2(part1Lines)}")
     }
-    println("Day 1 took ${t}ms to execute.")
+    println("Part 2 n^3 took: ${t3}")
+    val t4 = measureTimeMillis {
+        println("Part 2: ${part2WithHashset(part1Lines)}")
+    }
+    println("Part 2 w/ hashset took: ${t4}")
 }
 
 fun getLines(path: String): List<Int>{
@@ -59,6 +63,24 @@ fun part2(lines: List<Int>): Int {
                 if (number1.value + number2.value + number3.value== 2020) {
                     return number1.value * number2.value * number3.value
                 }
+            }
+        }
+    }
+    return 0
+}
+
+fun part2WithHashset(lines: List<Int>): Int {
+    val result = 2020
+    val linesHash = lines.toHashSet()
+
+    for(number1 in lines.withIndex()){
+        for(number2 in lines.withIndex()){
+            if(number1.index == number2.index) continue
+            if(number1.value + number2.value >= 2020) continue
+
+            val remaining = result - number1.value - number2.value
+            if(linesHash.contains(remaining)){
+                return number1.value * number2.value * remaining
             }
         }
     }
